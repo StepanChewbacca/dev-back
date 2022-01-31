43c1fbd699bbb2e3dcb1e40905cc2921b6e510cd
StepanChewbacca <qweqwe322322322@gmail.com>
2022-01-31 17:32:38 +0200

    Create ecosystem.config.js
    add pm2

---
+++ src/ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [{
+    name: 'server',
+    script: 'start ts-node -- -P tsconfig.json index.ts',
+    watch: '.',
+  },
+  ],
+
+  deploy: {
+    production: {
+      user: 'chewie',
+      host: '34.118.65.200',
+      ref: 'origin/prac1',
+      repo: 'https://github.com/StepanChewbacca/dev-back.git',
+      path: '/home/chewie/FirstDeploy',
+      ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
+      env: {
+        NODE_ENV: 'prac1',
+      },
+      'pre-setup': 'rm -fr /home/chewie/FirstDeploy',
+      'post-deploy': 'npm install && pm2 start ts-node -- -P tsconfig.json index.ts',
+    },
+  },
+};
