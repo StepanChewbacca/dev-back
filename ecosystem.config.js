0556a8ccc13e13adb5b34759eec68df622579ea1
StepanChewbacca <qweqwe322322322@gmail.com>
2022-02-01 15:42:52 +0200

    replace

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'server',
-    script: 'index.ts',
+    script: 'pm2 start ts-node -- -P tsconfig.json index.ts',
     watch: '.',
     env_production: {
       PORT: 8080,
